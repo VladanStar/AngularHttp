7b535c52aa822a16cad3a4ca9d80845f61911e4a
VladanStar <vladancupric@gmail.com>
2023-04-13 21:00:05 +0200

    Add

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { User } from '../model/user';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
   constructor(private http:HttpClient) { }
+getUsers() :Observable<User[]>{
+return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users')
+
+}
+getUser() :Observable<User>{
+  return this.http.get<User>('https://jsonplaceholder.typicode.com/user/1')
+
+  }
 }
