f3a0c19f6f5c2d13948a5e813e288f4dabbbde0e
VladanStar <vladancupric@gmail.com>
2023-04-13 19:53:49 +0200

    Add

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 }
