ce0396e83be07478d293b9c86d1d5cfe81fcdc6d
VladanStar <vladancupric@gmail.com>
2023-04-14 10:24:20 +0200

    Add

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,18 +4,19 @@ import { Observable, map } from 'rxjs';
 import { User } from '../model/user';
 import { environment } from 'src/environments/environment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-private apiUrl=environment.apiUrl;
+  private apiUrl = environment.apiUrl;
 
-  constructor(private http:HttpClient) { }
-getUsers() :Observable<User[]>{
-return this.http.get<User[]>(`${this.apiUrl}/users`)
-
-}
-getUser() :Observable<User>{
-  return this.http.get<User>(`${this.apiUrl}/users/1`)
-
+  constructor(private http: HttpClient) {}
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
+  }
+  getUser(): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/1`);
+  }
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}`, user);
   }
 }
