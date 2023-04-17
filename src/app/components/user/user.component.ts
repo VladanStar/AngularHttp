e38eca6c06fdaab7ee72149e34fb6612a52c3f48
VladanStar <vladancupric@gmail.com>
2023-04-17 09:13:43 +0200

    Add

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/model/user';
 import { UserService } from 'src/app/services/user.service';
 import { Geo } from 'src/app/model/geo';
+import { HttpEventType } from '@angular/common/http';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -24,7 +25,6 @@ export class UserComponent implements OnInit {
         lat: '233',
         lng: '233',
       },
-
     },
 
     phone: '1-770-736-8031 x56442',
@@ -37,11 +37,11 @@ export class UserComponent implements OnInit {
   };
 
   ngOnInit(): void {
-this.onPatchUser(this.user)
-     this.onGetUsers();
-//     this.onGetUser();
-// this.onCreateUser(this.user);
-// this.onUpdateUser(this.user)
+    this.onPatchUser(this.user);
+    this.onGetUsers();
+    //     this.onGetUser();
+    // this.onCreateUser(this.user);
+    // this.onUpdateUser(this.user)
   }
 
   onGetUsers() {
@@ -58,41 +58,53 @@ this.onPatchUser(this.user)
       () => console.log('Done geting users')
     );
   }
-  onCreateUser(user:User) {
+  onCreateUser(user: User) {
     this.userSevice.createUser(this.user).subscribe(
       (response) => console.log(response),
       (error) => console.log(error),
       () => console.log('Done creating users')
     );
   }
-  onUpdateUser(user:User) {
+  onUpdateUser(user: User) {
     this.userSevice.updateUser(this.user).subscribe(
       (response) => console.log(response),
       (error) => console.log(error),
       () => console.log('Done update users')
     );
   }
-  onPatchUser(user:User) {
+  onPatchUser(user: User) {
     this.userSevice.patchUser(this.user).subscribe(
       (response) => console.log(response),
       (error) => console.log(error),
       () => console.log('Done patch users')
     );
   }
-  deleteUser(id:number) {
+  deleteUser(id: number) {
     this.userSevice.deleteUser(id).subscribe(
       (response) => console.log(response),
       (error) => console.log(error),
       () => console.log('Done Delete users')
     );
   }
-  onUploadFile(files:File[]) {
-console.log(files);
-const formData= new FormData()
-    this.userSevice.deleteUser(5).subscribe(
-      (response) => console.log(response),
-      (error) => console.log(error),
-      () => console.log('Done Delete users')
+  onUploadFile(files: File[]): void {
+    console.log(files);
+    const formData = new FormData();
+    for (const file of files) {
+      formData.append('files', file, file.name);
+    }
+    this.userSevice.uploadfiles(formData).subscribe(
+      (event) => {
+        switch (event.type) {
+          case HttpEventType.UploadProgress || HttpEventType.DownloadProgress:
+            console.log(event);
+            break;
+          case HttpEventType.Response:
+            console.log(event);
+            break;
+        }
+      },
+      (error: any) => console.log(error),
+      () => console.log('Done delete user')
     );
   }
 }
